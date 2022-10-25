5094cfa5b52444cb765dc48c7ac8aad484115996
Valerii Stefanyk <valeriistefanyk@gmail.com>
2022-10-25 21:02:46 +0300

    Add creating person table

--- hw2_crud/main.py
+++ hw2_crud/main.py
@@ -10,11 +10,24 @@ def create_connection(db_path):
     return con
 
 
+def create_table(con):
+    sql = ''' CREATE TABLE IF NOT EXISTS person(
+       person_id INTEGER NOT NULL PRIMARY KEY,
+       first_name VARCHAR(128) NOT NULL,
+       last_name VARCHAR(128) NOT NULL,
+       address VARCHAR(1024),
+       jog VARCHAR(128),
+       age INTEGER
+    )
+    '''
+    con.execute(sql)
+
+
 def main():
     database = "data.db"
     con = create_connection(database)
     with con:
-        pass
+        create_table(con)
 
 
 if __name__ == "__main__":
